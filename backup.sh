378ba7d81a3a9d6493c5c9b10088fe3f7d5f2b67
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-02-01 13:39:32 +0800

    Update step order

--- backup.sh
+++ backup.sh
@@ -3,22 +3,24 @@ source /etc/profile
 source ./.env
 
 echo `date +"%Y-%m-%d %H:%M:%S"` " now starting backup"
-echo 'stop pleroma'
+
+echo "1.stop pleroma"
 sudo systemctl stop pleroma 
 
-echo "1.dump database"
+echo "2.dump database"
 sudo -Hu postgres pg_dump -d $PLEROMA_DB --format=custom -f ${BACKUP_PATH}/pleroma.pgdump
 
-echo "2.pack uploads & static folder"
+echo "3.pack uploads & static folder"
 tar -czf static.tar.gz  --absolute-names ${PLEROMA_PATH}/static 
 tar -czf uploads.tar.gz --absolute-names ${PLEROMA_PATH}/uploads}
 
-echo "3.copy config file"
+echo "4.copy config file"
 cp ${PLEROMA_CONFIG_PATH} ${BACKUP_PATH}
 
-echo "4.backup to remote"
+echo "5.backup to remote"
 ./duplicacy.sh
 
-echo "5.restart pleroma"
+echo "6.restart pleroma"
 sudo systemctl start pleroma 
+
 echo `date +"%Y-%m-%d %H:%M:%S"` " done!"
