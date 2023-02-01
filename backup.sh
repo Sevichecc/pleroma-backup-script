740505b3897115209a4b4c955f91b7cd9beedaff
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-02-01 13:25:27 +0800

    Update info

--- backup.sh
+++ backup.sh
@@ -6,7 +6,6 @@ echo `date +"%Y-%m-%d %H:%M:%S"` " now starting backup"
 echo 'stop pleroma'
 sudo systemctl stop pleroma 
 
-echo "————————————backup to local directory——————————"
 echo "1.dump database"
 sudo -Hu postgres pg_dump -d $PLEROMA_DB --format=custom -f ${BACKUP_PATH}/pleroma.pgdump
 
@@ -17,7 +16,6 @@ tar -czf uploads.tar.gz --absolute-names ${PLEROMA_PATH}/uploads}
 echo "3.copy config file"
 cp ${PLEROMA_CONFIG_PATH} ${BACKUP_PATH}
 
-echo "————————————upload to remote——————————"
 echo "4.backup to remote"
 ./duplicacy.sh
 
