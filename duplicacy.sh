3b1c7a4a8db21de6ffa20cadb3df2a991041de31
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-18 01:09:37 +0800

    Refactor code struture

--- duplicacy.sh
+++ duplicacy.sh
@@ -1,31 +1,32 @@
-#!/usr/bin/expect 
+#!/usr/bin/expect
 set ACCESS_KEY_ID "YOUR_ACCESS_KEY_ID"
 set SECRET_ACCESS_KEY "YOUR_SECRET_ACCESS_KEY"
 set PASSWORD "YOUR_PASSWORD"
 
 set timeout -1
+
+# Function to handle authentication
+proc authenticate {} {
+    global ACCESS_KEY_ID SECRET_ACCESS_KEY PASSWORD
+
+    expect "ID"
+    send "$ACCESS_KEY_ID\r"
+
+    expect "Secret"
+    send "$SECRET_ACCESS_KEY\r"
+
+    expect "password"
+    send "$PASSWORD\r"
+}
+
+# Run duplicacy backup command
 spawn duplicacy backup -threads 4
+authenticate
+expect "completed"
 
-expect "ID"
-send "$ACCESS_KEY_ID\r"
+# Run duplicacy prune command (optional)
+spawn duplicacy prune -keep 7:30
+authenticate
 
-expect "Secret"
-send "$SECRET_ACCESS_KEY\r"
-
-expect "password"
-send "$PASSWORD\r"
-
-##### (optional) Keep a revision every 7 days for revisions older than 30 days 
-# expect "completed"
-# spawn duplicacy prune -keep 7:30
-
-# expect "ID"
-# send "$ACCESS_KEY_ID\r"
-
-# expect "Secret"
-# send "$SECRET_ACCESS_KEY\r"
-
-# expect "password"
-# send "$PASSWORD\r"
-
-expect eof
+# Allow user interaction after script completion
+interact
