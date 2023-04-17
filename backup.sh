f72209fc633ba188f283838381a37379b78f7ab3
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-18 01:08:55 +0800

    fix: tar command can result in nested folder

--- backup.sh
+++ backup.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-source /etc/profile
+set -e
+source /etc/profile 
 source ./.env
 
-echo `date +"%Y-%m-%d %H:%M:%S"` " now starting backup"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+echo "(${TIMESTAMP}) now starting backup"
 
-echo "1.stop pleroma"
-sudo systemctl stop pleroma 
+echo "0.Deleting backups older than 7 days"
+find ${BACKUP_PATH} -type f -name '*.pgdump' -mtime +7 -exec rm {} \;
 
-echo "2.dump database"
-sudo -Hu postgres pg_dump -d $PLEROMA_DB --format=custom -f ${BACKUP_PATH}/pleroma.pgdump
+echo "1.Stopping pleroma"
+sudo systemctl stop pleroma  
 
-echo "3.pack uploads & static folder"
-tar -czf static.tar.gz  --absolute-names ${PLEROMA_PATH}/static 
-tar -czf uploads.tar.gz --absolute-names ${PLEROMA_PATH}/uploads
+echo "2.Dumping database"  
+sudo -Hu postgres pg_dump -d $PLEROMA_DB --format=custom -f ${BACKUP_PATH}/pleroma_${TIMESTAMP}.pgdump
 
-echo "4.copy config file"
-cp ${PLEROMA_CONFIG_PATH} ${BACKUP_PATH}
+echo "3.Packing uploads & static folder"
+tar -czf ${BACKUP_PATH}/static_${TIMESTAMP}.tar.gz -C ${PLEROMA_PATH} static  
+tar -czf ${BACKUP_PATH}/uploads_${TIMESTAMP}.tar.gz -C ${PLEROMA_PATH} uploads
 
-echo "5.backup to remote"
-./duplicacy.sh
+echo "4.Copying config file"
+cp ${PLEROMA_CONFIG_PATH} ${BACKUP_PATH}  
 
-echo "6.restart pleroma"
-sudo systemctl start pleroma 
+echo "5.Backuping to remote"
+./duplicacy.sh   
 
-echo `date +"%Y-%m-%d %H:%M:%S"` " done!"
+echo "6.Restarting pleroma"
+sudo systemctl start pleroma   
+
+echo "(${TIMESTAMP}) done!"
