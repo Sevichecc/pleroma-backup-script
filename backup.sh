1ef2e32aaabe05245e8a9843774fdcec57648319
SevicheCC <91365763+Sevichecc@users.noreply.github.com>
2023-05-29 10:00:21 +0800

    Refactor backup order

--- backup.sh
+++ backup.sh
@@ -6,9 +6,6 @@ source ./.env
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 echo "(${TIMESTAMP}) now starting backup"
 
-echo "0.Deleting backups older than 7 days"
-find ${BACKUP_PATH} -type f -name '*.pgdump' -mtime +7 -exec rm {} \;
-
 echo "1.Stopping pleroma"
 sudo systemctl stop pleroma  
 
@@ -19,13 +16,16 @@ echo "3.Packing uploads & static folder"
 tar -czf ${BACKUP_PATH}/static_${TIMESTAMP}.tar.gz -C ${PLEROMA_PATH} static  
 tar -czf ${BACKUP_PATH}/uploads_${TIMESTAMP}.tar.gz -C ${PLEROMA_PATH} uploads
 
-echo "4.Copying config file"
+echo "4.Restarting pleroma"
+sudo systemctl start pleroma   
+
+echo "5.Copying config file"
 cp ${PLEROMA_CONFIG_PATH} ${BACKUP_PATH}  
 
-echo "5.Backuping to remote"
+echo "6.Backuping to remote"
 ./duplicacy.sh   
 
-echo "6.Restarting pleroma"
-sudo systemctl start pleroma   
+echo "7.Deleting backups older than 7 days"
+find ${BACKUP_PATH} -type f -name '*.pgdump' -mtime +7 -exec rm {} \;
 
 echo "(${TIMESTAMP}) done!"
